7c80646efa168a4af7dc50f8f3f71292b4ff24c3
Nemanja Badnjarević <=>
2021-02-26 16:31:52 +0100

    Wheels

---
+++ yogamble/wheels/js/script.js
@@ -0,0 +1,74 @@
+(function() {
+  const wheel = document.querySelector('#wheel');
+  const startButton = document.querySelector('#button');
+  let deg = 0;
+  let cnt = 0;
+
+  startButton.addEventListener('click', () => {
+    startButton.style.pointerEvents = 'none';
+    deg = Math.floor(2500 + Math.random() * 2500);
+    console.log(cnt%2==0);
+    deg = deg - deg%30;
+    console.log(deg);
+    wheel.style.transition = 'all 5s ease-out';
+    wheel.style.transform = `rotate(${deg}deg)`;
+    ++cnt;
+  });
+
+  wheel.addEventListener('transitionend', () => {
+    startButton.style.pointerEvents = 'auto';
+    wheel.style.transition = 'none';
+    const actualDeg = deg % 360;
+    wheel.style.transform = `rotate(${actualDeg}deg)`;
+  });
+})();
+
+(function() {
+  const wheel = document.querySelector('#wheelLeft');
+  const startButton = document.querySelector('#buttonLeft');
+  let deg = 0;
+  let cnt = 0;
+
+  startButton.addEventListener('click', () => {
+    startButton.style.pointerEvents = 'none';
+    deg = Math.floor(2500 + Math.random() * 2500);
+    console.log(cnt%2==0);
+    deg = deg - deg%30;
+    console.log(deg);
+    wheel.style.transition = 'all 5s ease-out';
+    wheel.style.transform = `rotate(${deg}deg)`;
+    ++cnt;
+  });
+
+  wheel.addEventListener('transitionend', () => {
+    startButton.style.pointerEvents = 'auto';
+    wheel.style.transition = 'none';
+    const actualDeg = deg % 360;
+    wheel.style.transform = `rotate(${actualDeg}deg)`;
+  });
+})();
+
+(function() {
+  const wheel = document.querySelector('#wheelRight');
+  const startButton = document.querySelector('#buttonRight');
+  let deg = 0;
+  let cnt = 0;
+
+  startButton.addEventListener('click', () => {
+    startButton.style.pointerEvents = 'none';
+    deg = Math.floor(2500 + Math.random() * 2500);
+    console.log(cnt%2==0);
+    deg = deg - deg%30;
+    console.log(deg);
+    wheel.style.transition = 'all 5s ease-out';
+    wheel.style.transform = `rotate(${deg}deg)`;
+    ++cnt;
+  });
+
+  wheel.addEventListener('transitionend', () => {
+    startButton.style.pointerEvents = 'auto';
+    wheel.style.transition = 'none';
+    const actualDeg = deg % 360;
+    wheel.style.transform = `rotate(${actualDeg}deg)`;
+  });
+})();
